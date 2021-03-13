fb2269954353b05da6de50a1cd6d088a97698a8d
BetweenTheNumbers <851097+BetweenTheNumbers@users.noreply.github.com>
2021-03-13 15:24:35 -0500

    Add some comments

--- zillow.r
+++ zillow.r
@@ -6,19 +6,23 @@ rents <- read.csv('./data/Zip_Zri_AllHomesPlusMultifamily_Summary.csv', header =
 values$RegionName <- as.factor(values$RegionName)
 rents$RegionName <- as.factor(rents$RegionName)
 
+#group by RegionName
 merged <- merge(x = rents, y = values, by.x = 'RegionName', by.y = 'RegionName')
 
+#annualize market rents
 merged$X2020.01 <- merged$X2020.01 / 1000
 merged$Zri <- merged$Zri / 1000
 merged$VtoR <- merged$X2020.01 / (merged$Zri * 12)
 merged$County <- gsub(' County', '', merged$County)
 
+#for demo, take Baltimore, MD area counties
 selection <- merged[merged$State.x == 'MD' 
                     & merged$County %in% c('Howard', 'Anne Arundel', 'Harford', 'Baltimore City', 'Baltimore') 
                     & merged$X2020.01 <= 500,]
 
 mid <- median(selection$VtoR)
 
+#plot Rent Index vs. Value (Rent Ratio), implying Value is dependent
 ggplot(selection, aes(x = Zri * 1000, y = VtoR, col = County)) + 
   geom_text(aes(label = paste(City.x, ' ', RegionName)), size = 3.5) +
   geom_smooth(method = "lm", col = "black", size = 0.5) + 
